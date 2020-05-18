d0b2ca3059724a39a40a5de4e0bb3e289a958c63
Francois <frcugy@student.42.fr>
2020-05-18 14:58:34 +0200

    Update ajout du serveur node avec api, ajout de scores / getting scores
    Creation du serveur node JS
    routes API ->http://localhost:3000/GetScore
    (retourne un JSON contenant les info des top scores)
    
    + creation projet Unity

---
+++ Server/NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var fs = require("fs");
+var obj = {
+    users: []
+};
+
+app.get('/', (req, res) => res.send('Welcome to the ultimate ScoreBoard Project, there are 2 root that can give you either GetScore(no param) , or AddScore (2 param User;Score)<br/>Exemples :<br/>' +
+    'http://localhost:3000/GetScore' +
+    '<br/>'+
+    'http://localhost:3000/AddScore?user=toto&score=500'));
+
+app.get('/TestServer', (req, res) => {
+    res.send("ok");
+});
+
+app.get('/GetScore', (req, res) => {
+    fs.readFile('scores.json', 'utf8', function readFileCallback(err, data) {
+        if (err)
+            res.send("NO SCORE");
+        else
+            res.send(data);
+    });
+});
+
+app.get('/AddScore', (req, res) => {
+    fs.readFile('scores.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            obj.users.push({ user: req.query.user, score: req.query.score });
+            var json = JSON.stringify(obj);
+            fs.writeFile('scores.json', json, 'utf8', (res) =>{});
+        } else {
+            obj = JSON.parse(data); //now it an object
+            obj.users.push({ user: req.query.user, score: req.query.score }); 
+            var json = JSON.stringify(obj); 
+            fs.writeFile('scores.json', json, 'utf8', (res) => {
+            }); 
+        }
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
